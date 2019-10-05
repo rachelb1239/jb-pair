cde5522ac90c44cb2ea6536884e851d78cf22f79
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-10-05 11:02:21 -0600

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["react-hot", "babel-loader"],
+       }
+    ]
+  }
+};
