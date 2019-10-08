a88b386e0d946bf2688dd70048593ee8d9ec6b90
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-10-08 12:08:43 -0600

    font awesome installed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,13 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import '../../styles/custom.scss'
 
 const App = () => (
   <div>
     <h1>Hello World!</h1>
+    <FontAwesomeIcon icon={faCircleNotch} size="6x"/>
     <button type="button" className={`btn btn-primary`}>Test button</button>
   </div>
 )
