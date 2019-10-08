6e6a18edaa65e2b5d1f97e6cd5b73be9078efe65
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-10-08 11:32:44 -0600

    import all bootstrap styles

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,5 +2,11 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import '../../styles/custom.scss'
 
-const App = () => <h1>Hello World!</h1>;
+const App = () => (
+  <div>
+    <h1>Hello World!</h1>
+    <button type="button" className={`btn btn-primary`}>Test button</button>
+  </div>
+)
+
 export default hot(App);
